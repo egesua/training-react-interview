1fb2fd1640a288966e71ac57d6a798fa7d971e79
egesua <egesu06@gmail.com>
2024-04-20 21:31:27 +0300

    counter created.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
-    <div className="App">
-      
+    <div>
+      <h1>Hello egesua!</h1>
+      <h2>This is your interview training area.</h2>
+
+      <p>{counter}</p>
+
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Increase
+      </button>
+
+      <button
+        onClick={() => {
+          setCounter(counter - 1);
+        }}
+      >
+        Decrease
+      </button>
     </div>
   );
 }
