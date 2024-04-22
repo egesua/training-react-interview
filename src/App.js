9af6d46ca97cadb6a22869329129dfcd5e2c06a9
egesua <egesu06@gmail.com>
2024-04-22 12:20:52 +0300

    fetching the wanted attributes.

--- src/App.js
+++ src/App.js
@@ -5,18 +5,26 @@ import { useEffect, useState } from "react";
 function App() {
   const [counter, setCounter] = useState(0);
   const [randomUserDataJSON, setRandomUserDataJSON] = useState("");
-    
+  const [userInfo, setUserInfo] = useState([]);
+  const getFullUserName = (userInfo) => {
+    const {
+      name: { first, last },
+    } = userInfo;
+    return `${first} ${last}`;
+  };
+
   useEffect(() => {
     axios
       .get("https://randomuser.me/api")
-      .then(({data}) => {
+      .then(({ data }) => {
         console.log(data);
         setRandomUserDataJSON(JSON.stringify(data));
+        setUserInfo(data.results);
       })
       .catch((err) => {
         console.error(err);
       });
-  }, [])
+  }, []);
 
   return (
     <div className="App">
@@ -40,9 +48,15 @@ function App() {
       >
         Decrease
       </button>
-      <pre>
-        {randomUserDataJSON}
-      </pre>
+
+      {userInfo.map((userInfo, id) => (
+        <div key={id}>
+          <p>{getFullUserName(userInfo)}</p>
+          <img src={userInfo.picture.thumbnail} />
+        </div>
+      ))}
+
+      <pre>{randomUserDataJSON}</pre>
     </div>
   );
 }
